8a9b80da00b6720c72377ca8a6dcd1a84edf490f
piyush <130133404+git2piyush@users.noreply.github.com>
2023-08-30 09:49:31 +0000

    Update 5 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import "./styles.css";
-
+import {useDispatch, useSelector} from 'react-redux'
+import {decrement, increment, incrementByAmount} from './counter'
+interface RootState {
+  counter:{
+    count: number
+  }
+}
 export default function App() {
+  const {count} = useSelector((state: RootState)=> state.counter)
+  const dispatch = useDispatch()
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Count is: {count}</h1>
+      <button onClick={()=>dispatch(increment())}> Increment</button>
+      <button onClick={()=>dispatch(decrement())}>Decrement</button>
+      <button onClick={()=>dispatch(incrementByAmount(10))}>Add By 10 </button>
     </div>
   );
 }
